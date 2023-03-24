22a2f1f61ab290a13aad062b1046579cc11ec3ce
+ <+>
2023-03-24 14:00:54 +0300

    Fetch

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // To pass the tests, don't forget to return your fet
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((res)=>res.json())
+  .then((data)=> renderBooks(data))
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+
+  for(const book of books) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
-  });
+  }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
